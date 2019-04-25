6d9ef49e58a4646b3f44b43f20bc11218a1cdf49
prasetya2423 <prasetya2423@gmail.com>
2019-04-25 16:31:26 +0700

    Update Api.java

--- app/src/main/java/com/syncode/bacber/constanta/Api.java
+++ app/src/main/java/com/syncode/bacber/constanta/Api.java
@@ -3,5 +3,5 @@ package com.syncode.bacber.constanta;
 public class Api {
 
     public final static String BASE_URL = "https://newsapi.org/";
-    public final static String API_KEY = "b220bd692ce3453b9cfe3bae4c4197ee";
+    public final static String API_KEY = "YOUR API KEY";
 }
